2ad2bfbac59c82d7003ce3d1915ea288ddf3a169
Oksana Dziuba <dziuba131303@gmail.com>
2024-09-23 12:04:57 +0400

    front-end update

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ export const app = express();
 dotenv.config();
 
 const corsOptions = {
-  origin: "https://andea13.github.io/", // Replace with your frontend's actual origin
+  origin: "https://andea13.github.io/goit-react-hw-08-phonebook", // Replace with your frontend's actual origin
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true, // Allow credentials (cookies, authorization headers, etc.)
   allowedHeaders: ["Content-Type", "Authorization"], // Define allowed headers
